b8885cbceff1f3c2306cbed72a4c160d85856c6d
simon <valenciasimon99@gmail.com>
2024-10-11 23:37:18 -0500

    atributos privados, fuga, carga de datos

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_DesafioII_FILE C:/Users/HP/OneDrive/green/green/Ingenieria/II/Informatica II/Desafios/Desafío II/DesafioII/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/DesafioII.exe)
+set(__QT_DEPLOY_TARGET_DesafioII_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_DesafioII_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
